a06c4cab9ddac6fa37ffd6f2811c7659f664636e
Sabbir345 <sabbir.ahmad111@gmail.com>
2021-07-31 13:20:42 +0600

    fix design issue

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 //SLICK DLIDER
+var linkClicked = document.getElementsByClassName('nav-link');
+var numClass = linkClicked.length;
+
+for (var i = 0; i < numClass; i++) {
+        linkClicked[i].addEventListener('click', function(){
+      var onTheMoment = document.getElementsByClassName('active');
+            onTheMoment[0].className = onTheMoment[0].className.replace(' active', '');
+            this.className += ' active';
+    }, false);
+}
+
 $('.brand_slide').slick({
     centerMode: true,
     centerPadding: '0px',
